49482712f590e8994cbbe96a600bd58448ead5bd
Rocky-6 <imu387058@gmail.com>
2023-06-29 09:14:49 +0000

    jsonタグの修正

--- model/album.go
+++ model/album.go
@@ -2,9 +2,9 @@ package model
 
 type (
 	Album struct {
-		ID     int64   `json:id`
-		Title  string  `json:title`
-		Artist string  `json:artist`
-		Price  float32 `json:price`
+		ID     int64   `json:"id"`
+		Title  string  `json:"title"`
+		Artist string  `json:"artist"`
+		Price  float32 `json:"price"`
 	}
 )
--- model/healthz.go
+++ model/healthz.go
@@ -1,5 +1,5 @@
 package model
 
 type HealthzResponse struct {
-	Message string `json:message`
+	Message string `json:"message"`
 }
