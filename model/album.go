4077f2b89c159d1bedc8f0144ef60bb8d3e8b627
Rocky-6 <imu387058@gmail.com>
2023-06-30 04:55:11 +0000

    Albumに関するリクエストとレスポンスの定義

--- model/album.go
+++ model/album.go
@@ -7,4 +7,38 @@ type (
 		Artist string  `json:"artist"`
 		Price  float32 `json:"price"`
 	}
+
+	AddAlbumRequest struct {
+		Title  string  `json:"title"`
+		Artist string  `json:"artist"`
+		Price  float32 `json:"price"`
+	}
+
+	AddAlbumResponse struct {
+		Album Album `json:"album"`
+	}
+
+	GetAlbumByIDRequest struct {
+		ID int64 `json:"id"`
+	}
+
+	GetAlbumByIDResponse struct {
+		Album Album `json:"album"`
+	}
+
+	GetAlbumsByTitelRequest struct {
+		Title string `json:"title"`
+	}
+
+	GetAlbumsByTitleResponse struct {
+		Album []Album `json:"albums"`
+	}
+
+	GetAlbumsByArtistRequest struct {
+		Artist string `json:"artist"`
+	}
+
+	GetAlbumsByArtistResponse struct {
+		Album []Album `json:"albums"`
+	}
 )
